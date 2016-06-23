42a02ca4faf1d3059f0509d20f8708b0c7849523
alexws54tk <alex@ws54.tk>
2016-06-24 05:02:02 +0600

    Travis second clean

--- Documentation/9.regen_manifest.sh
+++ Documentation/9.regen_manifest.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-cd ..
-find -iname '*.ebuild' | { [[ $@ ]] && grep $@ || cat; } | xargs -I{} ebuild {} digest --force
+find ../ -type f -iname '*.ebuild' | { [[ $@ ]] && grep $@ || cat; } | xargs -I{} ebuild {} digest
